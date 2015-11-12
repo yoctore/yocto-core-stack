d77f02f787929f7812309c9153ae4cfb05d9c58b
Mathieu ROBERT <mathieu.robert@yocto.re>
2015-11-12 16:12:55 +0400

    MAj package

--- example/index.js
+++ example/index.js
@@ -9,13 +9,14 @@ core.debug = true;
 
 // Init your app first
 core.init().then(function () {
+  console.log('ala');
   // Init succeed start your app
   core.start().then(function () {
 
   /********************************************
    *              YOUR CODE HERE              *
    *******************************************/
-
+console.log('al');
   }).catch(function (error) {
     // default error use core logger
     core.logger.error([ '[ CoreWrapper.start ] -', error ].join(' '));
